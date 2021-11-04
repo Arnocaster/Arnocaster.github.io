6cd53c0ff00368c2403771fe417bd9f250d6f657
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2021-11-04 16:07:41 +0100

    ajout scroll souris horizontal

--- MRProto/js/eventListener.js
+++ MRProto/js/eventListener.js
@@ -35,7 +35,7 @@ const listeners =  {
             let scrollPx = 100;
             if (event.deltaY > 0 || Event.deltaX>0){
                 eltToScroll.scrollLeft += scrollPx;
-            } else if (event.deltaY < 0 || Event.deltaY<0){
+            } else if (event.deltaY < 0 || Event.deltaX<0){
                 eltToScroll.scrollLeft -= scrollPx;
             } else {
                 console.log("possible un mousewheel neutre?");
