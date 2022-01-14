4ae58255ba57a6d6d3712cb309effb2225d70b3f
Arnaud Peybernes <83549834+Arnocaster@users.noreply.github.com>
2022-01-14 17:05:32 +0100

    server

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -125,7 +125,7 @@ const app = {
 
 document.addEventListener('DOMContentLoaded', () => {
   //Socket.io Managment => Maybe a module later
-  const socket = io("http://lrwbtxw.cluster030.hosting.ovh.net/");
+  const socket = io("109.14.79.91:3000");
 
   socket.on('InitWorld', (obj) => {
     app.playerId = socket.id;
